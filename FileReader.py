b2dea666625880a3873478c456ac43ec8ab8051b
eyal <eyalh2001@gmail.com>
2021-11-10 12:03:04 +0200

    Main now reads the files and parses them to a list of calls and a dictionary representing a building

--- FileReader.py
+++ FileReader.py
@@ -1,33 +1,41 @@
 import json
+import csv
+
+"""
+This file has functions to read files and parse them to different data structures
+"""
 
 
-class FileReader:
+def read_file(path):
     """
-    Read files from csv and json formats
+    Read file given its path and parse depending on its type
+
+    :param path: The path to the file
+    :returns: The parsed file
     """
 
-    def __init__(self, path):
-        """
-        Create a file reader object with the path (with the file name and extension)
+    with open(path) as f:
+        if ".json" in path:
+            file = json.load(f)
 
-        :param path: The path to the file
-        """
+        elif ".csv" in path:
+            file = read_csv(f)
+        else:
+            raise FileNotFoundError("The file format is not supported")
 
-        with open(path) as f:
-            if ".json" in path:
-                self.file = json.load(f)
+        return file
 
-            elif ".csv" in path:
-                print("Got csv file will handle later")
 
-            else:
-                raise FileNotFoundError("The file format is not supported")
+def read_csv(file):
+    """
+    A custom csv file reader that turns the calls to a list of dictionaries
+    :param file: The file of the
+    :return: A list of dictionaries that represent the calls
+    """
 
-    def get_file(self):
-        """
-        Return the parsed file stored in this FileReader object
+    calls = file.read().split("\n")
 
-        :return: The parsed file
-        """
-        return self.file
+    return calls
+
+
 
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from Building import Building
-from Simulator import Simulator
-from Elevator import Elevator
-from FileReader import FileReader
+import FileReader
+import sys
+import os
 
+BUILDING_DIR_PATH = "data/Ex1_input/Ex1_Buildings"
+CALL_DIR_PATH = "data/Ex1_input/Ex1_Calls"
 
-fr = FileReader("data/Ex1_input/Ex1_Buildings/B2.json")
+building_path = os.path.join(BUILDING_DIR_PATH, sys.argv[1])
+call_path = os.path.join(CALL_DIR_PATH, sys.argv[2])
 
-building_dict = fr.get_file()
+building_dir = FileReader.read_file(building_path)
+calls_list = FileReader.read_file(call_path)
 
-for key in building_dict:
-    print("{}: {}".format(key, building_dict[key]))
-
-
+print(building_dir)
+print(calls_list)
 
